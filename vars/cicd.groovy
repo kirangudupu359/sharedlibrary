17b7b9fa685364bc11b4aee9638c18aa1e750845
Kiran <kirangudupu359@gmail.com>
2025-06-22 14:57:32 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -13,7 +13,7 @@ def Deployment(name,ipaddress,location)
  sh "scp /var/lib/jenkins/workspace/${name}/webapp/target/webapp.war ubuntu@${ipaddress}:/var/lib/tomcat10/webapps/${location}"
 }
 
-def Testing(name)
+def runselenium(name)
 {
  sh "java -jar  /var/lib/jenkins/workspace/${name}/testing.jar"
 }
